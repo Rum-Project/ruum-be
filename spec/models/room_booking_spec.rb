7b790e0ecb6e8a0289495c42b74acba79783a765
Chris Hewitt <agop5134@gmail.com>
2022-03-24 17:13:42 -0600

    validation spec for RoomBooking

--- spec/models/room_booking_spec.rb
+++ spec/models/room_booking_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe RoomBooking, type: :model do
     it {should belong_to :room}
     it {should belong_to :booking}
   end
+
+  describe "validations" do
+    it {should validate_presence_of :room_id}
+    it {should validate_presence_of :booking_id}
+  end
 end
