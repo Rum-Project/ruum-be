dd77cf0b5de657afc475ad29b6f5b7dd19e32f0c
Chris Hewitt <agop5134@gmail.com>
2022-03-24 17:44:23 -0600

    room validations

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -5,4 +5,22 @@ RSpec.describe Room, type: :model do
   it {should belong_to :host}
   it {should have_many :room_bookings}
   end
+
+  describe "validations" do
+    it {should validate_presence_of(:host_id)}
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:details)}
+    it {should validate_presence_of(:photo)}
+    it {should validate_presence_of(:address)}
+    it {should validate_presence_of(:city)}
+    it {should validate_presence_of(:state)}
+    it {should validate_presence_of(:zip)}
+    it {should validate_presence_of(:state)}
+    it {should validate_presence_of(:price)}
+    it {should validate_numericality_of(:price)}
+    it {should validate_presence_of(:capacity)}
+    it {should validate_numericality_of(:capacity)}
+    it {should validate_presence_of(:amenities)}
+    it {should validate_presence_of(:instruments)}
+  end
 end
