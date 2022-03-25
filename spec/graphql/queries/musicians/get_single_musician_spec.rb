13695483b17192ee2112a06104406fbbd5472290
Hannah Warren <hannahkwarren@gmail.com>
2022-03-24 21:34:50 -0400

    Fix get single musician query

--- spec/graphql/queries/musicians/get_single_musician_spec.rb
+++ spec/graphql/queries/musicians/get_single_musician_spec.rb
@@ -1,23 +1,24 @@
+# spec/graphql/queries/musicians/get_single_musician_spec.rb
+
 require 'rails_helper'
 
 RSpec.describe Types::QueryType do
   describe 'display musicians' do
     it 'can query a single musician' do
-      Musician.create(id: 5, name: "Bruce Easley", email: "bruce.e@mail.com", phone: "7573830582")
-      Musician.create(id: 6, name: "Bruce Isley", email: "bruce.i@mail.com", phone: "9573830582")
+      Musician.create!(id: 50, name: 'Bruce Easley', email: 'bruce.e@mail.com', phone: '7573830582', photo: 'www.photo.com')
+      Musician.create!(id: 60, name: 'Bruce Isley', email: 'bruce.i@mail.com', phone: '9573830582', photo: 'www.photo.com')
 
       result = RuumBeSchema.execute(query).as_json
-
-      expect(result["data"]["getMusician"]["name"]).to eq("Bruce Easley")
-      expect(result["data"]["getMusician"]["email"]).to eq("bruce.e@mail.com")
-      expect(result["data"]["getMusician"]["phone"]).to eq("7573830582")
+      expect(result['data']['getMusician']['name']).to eq('Bruce Easley')
+      expect(result['data']['getMusician']['email']).to eq('bruce.e@mail.com')
+      expect(result['data']['getMusician']['phone']).to eq('7573830582')
     end
   end
 
   def query
     <<~GQL
     {
-      getMusician(id: "5") {
+      getMusician(id: "50") {
         name
         email
         phone
