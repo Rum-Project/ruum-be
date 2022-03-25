cf41692893cc092e3de92f6df7b72114fd3b97cc
Chris Hewitt <agop5134@gmail.com>
2022-03-24 18:19:36 -0600

    create musician model validations and spec

--- app/models/musician.rb
+++ app/models/musician.rb
@@ -1,3 +1,19 @@
 class Musician < ApplicationRecord
   has_many :bookings
+
+  validates :name, presence: true
+
+
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :photo, presence: true
+
+  VALID_PHONE_NUMBER_REGEX = /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
+  validates :phone,
+            presence: true,
+            length: {maximum: 15},
+            format: { with: VALID_PHONE_NUMBER_REGEX }
 end
