850f37653cb909a82c4903862994315193ebf674
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-31 13:47:14 -0600

    Add mutation for musician destroy

--- app/graphql/mutations/destroy_musician.rb
+++ app/graphql/mutations/destroy_musician.rb
@@ -1,15 +1,17 @@
-# app/graphql/mutations/create_musician.rb
+# app/graphql/mutations/destroy_musician.rb
 
 module Mutations
-  class DeleteMusician < BaseMutation
+  class DestroyMusician < BaseMutation
     argument :id, ID, required: true
-    # argument :musician_id, ID, required: true
 
     type Types::MusicianType
 
     def resolve(id: nil)
       Booking.where(musician_id: id).destroy_all
       Musician.destroy(id)
+
+    rescue ActiveRecord::RecordNotFound => e
+      GraphQL::ExecutionError.new("Invalid input: #{e.message}")
     end
   end
 end
