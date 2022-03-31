0048cdbfe126b72b63fa54b45768b50fab092019
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-31 13:46:12 -0600

    Change deleteMusician to destroyMusician

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,7 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :destroy_booking, mutation: Mutations::DestroyBooking
     field :create_musician, mutation: Mutations::CreateMusician
-    field :delete_musician, mutation: Mutations::DeleteMusician
+    field :destroy_musician, mutation: Mutations::DestroyMusician
     field :update_musician, mutation: Mutations::UpdateMusician
     field :create_booking, mutation: Mutations::CreateBooking
   end
