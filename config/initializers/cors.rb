bc06c20379d4e731f04e7ac25dd6505f351acd10
Hannah Warren <hannahkwarren@gmail.com>
2022-04-04 12:40:39 -0400

    Attempt to fix cors errors by removing regex from origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,7 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000', 'https://ruumproject.herokuapp.com'
