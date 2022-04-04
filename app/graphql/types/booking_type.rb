cef14f2b2a839863c7116f9d39a1e52adc482834
Hannah Warren <hannahkwarren@gmail.com>
2022-04-04 17:01:59 -0400

    Removes unused method on Booking type and empty bookings spec folder

--- app/graphql/types/booking_type.rb
+++ app/graphql/types/booking_type.rb
@@ -6,10 +6,4 @@ module Types
     field :musician, Types::MusicianType, null: false
     field :room, Types::RoomType, null: false
   end
-
-  def room
-    return if (object.instance_of? Room) || object.room_id.nil?
-
-    Room.find(object.booking_id)
-  end
 end
